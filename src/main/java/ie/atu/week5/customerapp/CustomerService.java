711ce4f8c5d8e4f6490e8763b1aefb5d993313e8
Mikaela <G00401392@atu.ie>
2024-10-14 17:00:55 +0100

    WIP CustomerService

--- src/main/java/ie/atu/week5/customerapp/CustomerService.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerService.java
@@ -1,4 +1,19 @@
 package ie.atu.week5.customerapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class CustomerService {
+
+    private final OrderRepository orderRepository;
+
+    public CustomerService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Customer> getCustomers()
+    {
+        return orderRepository.findAll();
+    }
 }
