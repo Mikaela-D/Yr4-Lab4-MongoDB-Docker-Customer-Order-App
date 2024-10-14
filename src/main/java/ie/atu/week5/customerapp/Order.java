18624d65eaacbccd9d702f76b1401a9d2c3072bf
Mikaela <G00401392@atu.ie>
2024-10-14 14:25:24 +0100

    Added new project

---
+++ src/main/java/ie/atu/week5/customerapp/Order.java
@@ -0,0 +1,18 @@
+package ie.atu.week5.customerapp;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "orders")
+public class Order {
+
+    @Id
+    private String id;
+    private int orderCode;
+    private String orderDetails;
+    private String orderDate;
+    private String customerId; // Reference to the associated customer
+}
