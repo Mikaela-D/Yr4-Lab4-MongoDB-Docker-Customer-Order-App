081551e8ea91c4e729a8de08bf3dbc22935a45d4
Mikaela <G00401392@atu.ie>
2024-10-18 20:37:56 +0100

    Moved service logic to a separate service class

--- src/main/java/ie/atu/week5/customerapp/CustomerService.java
+++ src/main/java/ie/atu/week5/customerapp/CustomerService.java
@@ -1,19 +1,38 @@
 package ie.atu.week5.customerapp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class CustomerService {
 
-    private final OrderRepository orderRepository;
+    private final CustomerRepository customerRepository;
 
-    public CustomerService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
-    public List<Customer> getCustomers()
-    {
-        return orderRepository.findAll();
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public Optional<Customer> getCustomerById(String id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public boolean customerExists(String id) {
+        return customerRepository.existsById(id);
+    }
+
+    public void deleteCustomer(String id) {
+        customerRepository.deleteById(id);
     }
 }
